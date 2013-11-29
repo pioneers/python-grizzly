861f0a9343e8ad6b6aa639690601709821f00ca6
Felix Li <felixli@pioneers.berkeley.edu>
2013-11-29 00:57:56 -0800

    GrizzlyDriver: Initial port of features from PiER drivers

--- grizzly_drivers/libgrizzlyusb.py
+++ grizzly_drivers/libgrizzlyusb.py
@@ -1,5 +1,6 @@
 import usb.core
 import struct
+from __future__ import division
 
 class GrizzlyUSB(object):
 	def __init__(self, idVendor = 0x03eb, idProduct=0x204f):
@@ -26,16 +27,15 @@ class GrizzlyUSB(object):
 		
 class Grizzly(object):
 	COMMAND_ENABLE_USB_MODE				= "\x9A\x81\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
-	COMMAND_DISABLE_TIMEOUT             = "\x80\x82\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
 
 	def __init__(self, device):
 		self._dev = device
-		self._dev.send_bytes(Grizzly.COMMAND_ENABLE_USB_MODE)
-		self._dev.send_bytes(Grizzly.COMMAND_DISABLE_TIMEOUT)
+		self._ticks = 0
+		#self._dev.send_bytes(Grizzly.COMMAND_ENABLE_USB_MODE)
 	
-	def set_register(self, addr, numBytes, data):
-		assert numBytes <= 14, "Cannot write more than 14 bytes at a time"
-		cmd = chr(addr) + chr(numBytes | 0x80)
+	def set_register(self, addr, data):
+		assert len(data) <= 14, "Cannot write more than 14 bytes at a time"
+		cmd = chr(addr) + chr(len(data) | 0x80)
 		for byte in data:
 			cmd += chr(byte)
 		cmd += (16 - len(cmd)) * chr(0)
@@ -52,7 +52,56 @@ class Grizzly(object):
 	
 	def setSpeed(self, setpoint):
 		buf = [0, 0, setpoint, 0, 0]
-		self.set_register(Addr.Speed, 5, buf)
+		self.set_register(Addr.Speed, buf)
+		
+	def _read_as_int(self, addr, numBytes):
+	    buf = self.read_register(addr, numBytes)
+	    rtn = 0
+	    for i, byte in enumerate(buf):
+	        rtn |= byte << 8 * i
+	    return rtn
+    
+    def _set_as_int(self, addr, val, numBytes = 1):
+        buf = []
+        for i in range(numBytes):
+            buf.append((val >> 8 * i) & 0xff)
+        self.set_register(addr, buf)
+	
+	def MotorCurrent():
+        return _read_as_int(Addr.MotorCurrent, 2)
+    
+    def GetTimeout():
+        return _read_as_int(Addr.Timeout, 2)
+    
+    def ReadEncoder():
+        return _read_as_int(Addr.EncoderCount, 4)
+        
+    def hasReset():
+        currentTime = _read_as_int(Addr.Uptime, 4)
+        if currentTime <= self._ticks:
+            return True
+        return False
+    
+    def LimitAcceleration(accel):
+        if accel > 0x8f:
+            raise Exception("Acceleration limit cannot exceed 143")
+        self._set_as_int(Addr.AccelLimit, accel)
+        
+    def LimitCurrent(curr):
+        self._set_as_int(Addr.CurrentLimit, curr)
+        
+    def InitPID(kp, ki, kd):
+        p, i, d = map(lambda x: x * (2 ** 16), (kp, ki, kd))
+        self._set_as_int(Addr.PConstant, p)
+        self._set_as_int(Addr.IConstant, i)
+        self._set_as_int(Addr.DConstant, d)
+        
+    def GetPIDConstants():
+        p = self._read_as_int(Addr.PConstant, 4)
+        i = self._read_as_int(Addr.IConstant, 4)
+        d = self._read_as_int(Addr.DConstant, 4)
+        
+        return map(lambda x: x / (2 ** 16), (p, i, d))
 
 class ControlMode(object):
 	NO_PID 							= 0x02
@@ -73,5 +122,6 @@ class Addr(object):
 	IConstant						= 0x34
 	DConstant						= 0x38
 	Timeout							= 0x80
+	CurrentLimit                    = 0x82
 	AccelLimit						= 0x90
 	Uptime							= 0x94
